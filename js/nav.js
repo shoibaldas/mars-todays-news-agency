36ece9f9266a418c447b8454adf1527535d3f1ad
shoibaldas <shoibalshantu@gmail.com>
2022-09-03 00:22:37 +0600

    navbar added

---
+++ js/nav.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle").addEventListener("click", function () {
+    document.getElementById("nav").classList.toggle("-right-full")
+    document.getElementById("nav").classList.toggle("right-0")
+})
