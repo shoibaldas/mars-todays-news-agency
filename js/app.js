fe09f6c8593eb4723ab42f63edbec2d2e41891a4
shoibaldas <shoibalshantu@gmail.com>
2022-09-03 21:12:10 +0600

    default homepage data display complete

--- js/app.js
+++ js/app.js
@@ -3,13 +3,11 @@ const newsCategories = async () => {
         const url = 'https://openapi.programming-hero.com/api/news/categories';
         const res = await fetch(url);
         const data = await res.json();
-
         //return data;
         displayNewsCategories(data.data.news_category);
     } catch (error) {
         console.log("Bad Error!");
     }
-
 }
 
 const displayNewsCategories = (data) => {
@@ -32,7 +30,6 @@ const getNewsFromCategories = async (id, element) => {
         const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
         const res = await fetch(url);
         const data = await res.json();
-
         //displayNewsByCategory(data.data, element);
         showNewsCat(data.data, element);
         //console.log(element);
@@ -45,13 +42,12 @@ const showNews = (category_id, element) => {
     //console.log(element);
     loadSpinner(true);
     getNewsFromCategories(category_id, element);
-
-
 }
 
 const checkAvailableNews = (id, element) => {
     const getNewsItemContainer = document.getElementById('news-item');
     getNewsItemContainer.innerHTML = '';
+
     const item = document.getElementById('news-item');
     if (element) {
         item.classList.remove('hidden');
@@ -61,23 +57,19 @@ const checkAvailableNews = (id, element) => {
     }
     const newsDiv = document.createElement('div');
     newsDiv.innerHTML = `
-    <p class="text-gray-600 text-4xl font-semibold">${id} items found in category ${element}</p>`;
+    <p class="text-gray-600 text-3xl font-semibold">${id} items found in category ${element}</p>`;
 
     getNewsItemContainer.appendChild(newsDiv);
 }
 
 const showNewsCat = (data, element) => {
-
     const newses = [];
     for (const news of data) {
         newses.push(news);
     }
-
-    newses.sort((a, b) => b.total_view - a.total_view)
-
+    newses.sort((a, b) => b.total_view - a.total_view);
     data = newses;
     //console.log(data);
-
     displayNewsByCategory(data, element);
 }
 
@@ -102,23 +94,22 @@ const displayNewsByCategory = (data, element) => {
         //const { number } = rating;
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('col');
-
         newsDiv.innerHTML = `
         <div
                 class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-full hover:bg-gray-100">
                 <img class="object-cover w-full h-96 rounded-t-lg p-4 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                    src="${thumbnail_url}" alt="">
+                    src="${thumbnail_url ? thumbnail_url : "No data Available"}" alt="">
                 <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-600">${title}</h5>
-                    <p class="mb-3 font-normal text-gray-400">${details.slice(0, 400)}...</p>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-600">${title ? title : "No data Available"}</h5>
+                    <p class="mb-3 font-normal text-gray-400">${details.slice(0, 280)}...</p>
                     <div class="flex justify-between items-center">
                         <div class="flex">
                             <div>
-                                <img class="object-contain w-12 rounded-full" src="${img}" alt="">
+                                <img class="object-contain w-12 rounded-full" src="${img ? img : "No data Available"}" alt="">
                             </div>
                             <div class="ml-2">
                                 <p class="font-semibold">${name ? name : "No data Available"}</p>
-                                <p class="text-gray-400">${published_date}</p>
+                                <p class="text-gray-400">${published_date ? published_date : "No data Available"}</p>
                             </div>
                         </div>
                         <div class="ml-2">
@@ -140,7 +131,6 @@ const displayNewsByCategory = (data, element) => {
         `;
         getIdByCategory.appendChild(newsDiv);
     })
-
     loadSpinner(false);
 }
 
@@ -158,7 +148,6 @@ const modalDataView = async (news_id) => {
 const modalDataAdd = (data) => {
     const { author, thumbnail_url, title, details } = data;
     const { img, name, published_date } = author;
-
     const getData = document.getElementById('modal-view');
     getData.textContent = '';
     const newsDiv = document.createElement('div');
@@ -166,20 +155,19 @@ const modalDataAdd = (data) => {
     newsDiv.innerHTML = `
                     <img class="object-cover w-full h-96 rounded-t-lg p-4 md:h-2/5 md:w-full md:rounded-none md:rounded-l-lg"
                     src="${thumbnail_url}" alt="">
-                    <h5 class="mt-2 text-2xl font-bold tracking-tight text-gray-200">${title}</h5>
+                    <h5 class="mt-2 text-2xl font-bold tracking-tight text-gray-200">${title ? title : "No data Available"}</h5>
                 <div class="flex items-center mt-4 mb-4">
                     <div>
-                        <img class="object-contain w-12 rounded-full" src="${img}" alt="">
+                        <img class="object-contain w-12 rounded-full" src="${img ? img : "No data Available"}" alt="">
                     </div>
                     <div class="ml-2">
                         <p class="font-semibold">${name ? name : "No data Available"}</p>
-                        <p class="text-gray-400">${published_date}</p>
+                        <p class="text-gray-400">${published_date ? published_date : "No data Available"}</p>
                     </div>
                     
                 </div>
                 <p class="mb-3 font-normal text-justify text-gray-200">${details}</p>
     `;
-
     getData.appendChild(newsDiv);
     loadSpinner(false);
 }
@@ -196,6 +184,6 @@ const loadSpinner = (isLoad) => {
 }
 
 newsCategories();
-//getNewsFromCategories();
+getNewsFromCategories("08", "All News");
 //modalDataView();
 //checkAvailableNews();
