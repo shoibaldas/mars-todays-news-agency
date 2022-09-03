60770cd8455daabb5f60bcd3e215205c7dac5335
shoibaldas <shoibalshantu@gmail.com>
2022-09-04 00:25:17 +0600

    correction

--- js/nav.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("toggle").addEventListener("click", function () {
-    document.getElementById("nav").classList.toggle("-right-full")
-    document.getElementById("nav").classList.toggle("right-0")
-})
